28831ebd2842774e2448c553b9bf428e9e15d418
Melina Garza <mjg2290@columbia.edu>
2024-10-26 19:36:26 -0400

    sending the pic

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, request, send_file, jsonify, g
+from flask import Flask, request, send_file, jsonify
 from flask_cors import CORS, cross_origin
 import lightkurve as lk
 import numpy as np
 from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
-import logging
+import matplotlib
+matplotlib.use('agg')
+from PIL import Image
+import io
+from base64 import encodebytes
+import logging, os
 
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
@@ -17,13 +22,14 @@ def hello_world():
 @app.route('/create_curve', methods=['POST'])
 @cross_origin(origin='*')
 def create_curve():
+    
     data = request.form
     name = data['lightcurve'].split(',')[0]
     author = "Kepler"
     quarter = data['lightcurve'].split(',')[1]
    
     
-    #processing the lighcurve
+    #processing the lightcurve
     lc = lk.search_lightcurve(name, author=author, quarter=int(quarter)).download()
     pg = lc.normalize(unit='ppm').to_periodogram()
     power = []
@@ -32,8 +38,18 @@ def create_curve():
     
     x = np.array(power)
     peaks, _ = find_peaks(x, distance=400)
+    plt.clf() 
+    plt.plot(x)
+    plt.plot(peaks, x[peaks], "x")
+    plt.plot(np.zeros_like(x), "--", color="gray")
+    plt.savefig('lightcurve-frequency.png')
 
-    return jsonify( {'frequencies': list(x[peaks])})
+    pil_img = Image.open('lightcurve-frequency.png', mode='r') # reads the PIL image
+    byte_arr = io.BytesIO()
+    pil_img.save(byte_arr, format='PNG') # convert the PIL image to byte array
+    encoded_img = encodebytes(byte_arr.getvalue()).decode('ascii') # encode as base64
+
+    return jsonify( {'frequencies': list(x[peaks]), 'ImageBytes': encoded_img})
 
 if __name__ == '__main__':  
    app.run(debug=True)
