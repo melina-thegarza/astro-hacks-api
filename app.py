e155812e3618e133a5d351faf07f2a185c816670
Eva <ems2333@columbia.edu>
2024-10-26 14:41:41 -0400

    return send_file('my_plot.png'

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, send
+import matplotlib
+import lightkurve as lk
+import scipy
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -9,9 +13,26 @@ def hello_world():
 @app.route('/create_curve', methods=['POST'])
 def create_curve():
     data = request.form
-    # process the form
-    return data
-
+    
+    #processing the lighcurve
+    search_result = lk.search_lightcurve("V1357 Cyg")
+    lc = search_result[0].download()
+    pg = lc.normalize(unit='ppm').to_periodogram()
+    
+    #returning plot
+    pg. plot() 
+    plt.savefig('my_plot.png')
+    
+    
+    #returning the array of peaks
+    pow=pg.power
+    power_array= np.zeros(12000)
+    k=0
+    for i in pow:
+        power_array[k]=i.value
+        k=k+1
+    peaks=scipy.signal.find_peaks(power_array, height=0, distance=350)
+    return jsonify(peaks), send_file('my_plot.png', mimetype='image/gif')
 
 if __name__ == '__main__':  
-   app.run() 
+   app.run()
